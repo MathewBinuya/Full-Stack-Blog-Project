3ed2e741b4085927f4a1119f0585948bf226a4bb
CodewithPixie <xzocantsleep@gmail.com>
2026-01-14 19:52:09 +0800

    user login & user logout fix

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -71,14 +71,40 @@ const loginUser = async (req, res) => {
 
   } catch (error) {
     res.status(500).json({
-      message: "Internal Server Error"
+      message: "Internal Server Error", error
     });
 
 
   }
 }
 
+
+const logoutUser = async (req, res) => {
+  try {
+    const {email , password} = req.body
+    const user = await User.findOne({
+      email
+    })
+
+    if(!user) return res.status(404).json({
+      message: "User not found"
+    });
+
+    res.status(200).json({
+      message: "Logout sucessful"
+    });
+
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error", error
+    })
+  }
+}
+
+
 export {
   registerUser,
-  loginUser
+  loginUser,
+  logoutUser
 }
