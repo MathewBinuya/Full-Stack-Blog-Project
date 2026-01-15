b839d1d7b3a3675dbde7127001a21713e4a266d6
CodewithPixie <xzocantsleep@gmail.com>
2026-01-15 21:38:59 +0800

    frontend connected

---
+++ frontend/login/login.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('loginForm');
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  try {
+    const res = await fetch("http://localhost:4000/api/v1/users/login", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({email, password})
+   });
+
+
+   const data = await res.json();
+
+   if(!res.ok) {
+    alert(data.message);
+    return;
+   }
+
+  localStorage.setItem("token", data.token);
+
+  window.location.href = "home.html";
+
+  } catch (error) {
+    console.error("Login error: ", error);
+    alert("Please try again later.");
+
+  }
+
+});
---
+++ frontend/register/register.js
@@ -0,0 +1,40 @@
+const form = document.getElementById("registerForm");
+
+    // Listen for from submit
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email  = document.getElementById('email').value.trim();
+  const password = document.getElementById('password').value.trim();
+
+   // Basic validation
+  if(!email || !password) {
+    alert("Email and password are required");
+    return;
+  }
+
+  try {
+    // Send request to backend
+    const res = await fetch("http://localhost:4000/api/v1/users/register", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({email, password})
+    });
+
+      const data = await res.json();
+
+      if(!res.ok) {
+        alert(data.message || "Registration failed");
+        return;
+      }
+
+      alert("Registration sucessful! Please login.");
+
+      window.location.href = "login.html";
+
+
+  } catch (error) {
+      console.error("Register error:", error);
+      alert("Server error. Please try again later.");
+  }
+});
